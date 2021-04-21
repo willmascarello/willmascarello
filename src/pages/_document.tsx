eee4ced95e025fa53d13b3640d85bcec236ad62e
Will Mascarello <william.mascarello@duo.studio>
2021-04-21 11:01:07 -0300

    Menu: Animações e links

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default class MyDocument extends Document {
     return(
       <Html>
         <Head>
-          <link rel="shortcut icon" href="logo_color.svg" type="image/svg"/>
+          <link rel="shortcut icon" href="logo.svg" type="image/svg"/>
 
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Stoke&display=swap" rel="stylesheet" />
