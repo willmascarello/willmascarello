afb5a68a57977fd6399008442a1b589760a76694
Will Mascarello <william.mascarello@duo.studio>
2021-04-20 21:31:40 -0300

    Home: Estilização de titulo e subtitulo

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -14,9 +14,11 @@ export function HomePage() {
             <video autoPlay loop muted className={styles.videoBackground}>
                 <source src="video/Mountains.mp4" type="video/mp4" />
             </video>
-            <h2>Olá,</h2>
-            <h1>Sou o William Mascarello</h1>
-            <h2>Bem vindo ao Meu Universo em constante evolução</h2>
+            <div className={styles.title}>
+                <h2>Olá,</h2>
+                <h1>Sou o William Mascarello</h1>
+            </div>
+            <h2 className={styles.subtitle}>Bem vindo ao <br/>Meu Universo <br/>em constante evolução</h2>
         </div>
     )
 }
