952ba25158baadead2421ffd4cf11011cffb89b3
William Mascarello <william.mascarello@fcid.com.br>
2021-04-22 11:34:08 -0300

    Adicionado elemento lang no Html

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, {Html, Head, Main, NextScript} from 'next/document';
 export default class MyDocument extends Document {
   render () {
     return(
-      <Html>
+      <Html lang="pt-br">
         <Head>
           <link rel="shortcut icon" href="logo.svg" type="image/svg"/>
 
