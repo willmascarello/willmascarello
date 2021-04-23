3b0a90ae7fff989fc15364b95b6946357929cd7a
William Mascarello <william.mascarello@fcid.com.br>
2021-04-23 15:30:52 -0300

    Alterado icone do player e ordem dos contatos

--- src/components/Music.tsx
+++ src/components/Music.tsx
@@ -1,8 +1,36 @@
 import React, { useState, useEffect } from "react";
+import styles from "../styles/components/Music.module.css"
 
-const iconPlay = <svg aria-label="Play" focusable="false" data-prefix="fas" data-icon="play" className="svg-inline--fa fa-play fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#F8F8F870" d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z"></path></svg>;
+const iconPause = <div className={styles.bars}>
+                    <div className={styles.dots}>
+                    <div className={styles.dot__1}></div>
+                    <div className={styles.dot__2}></div>
+                    <div className={styles.dot__3}></div>
+                    <div className={styles.dot__4}></div>
+                    <div className={styles.dot__5}></div>
+                    <div className={styles.dot__6}></div>
+                  </div>
+                  <div className={styles.dots}>
+                    <div className={styles.dot__1r}></div>
+                    <div className={styles.dot__2r}></div>
+                    <div className={styles.dot__3r}></div>
+                    <div className={styles.dot__4r}></div>
+                    <div className={styles.dot__5r}></div>
+                    <div className={styles.dot__6r}></div>
+                  </div>
+                </div>;
 
-const iconPause = <svg aria-label="Pause" focusable="false" data-prefix="fas" data-icon="pause" className="svg-inline--fa fa-pause fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#F8F8F870" d="M144 479H48c-26.5 0-48-21.5-48-48V79c0-26.5 21.5-48 48-48h96c26.5 0 48 21.5 48 48v352c0 26.5-21.5 48-48 48zm304-48V79c0-26.5-21.5-48-48-48h-96c-26.5 0-48 21.5-48 48v352c0 26.5 21.5 48 48 48h96c26.5 0 48-21.5 48-48z"></path></svg>;
+const iconPlay = <div className={styles.bars}>
+                        <svg aria-label="Play" focusable="false" data-prefix="fas" data-icon="play" className={styles.play__icon} role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#F8F8F820" d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z"></path></svg>
+                        <div className={styles.dots}>
+                          <div className={styles.dot__1p}></div>
+                          <div className={styles.dot__2p}></div>
+                          <div className={styles.dot__3p}></div>
+                          <div className={styles.dot__4p}></div>
+                          <div className={styles.dot__5p}></div>
+                          <div className={styles.dot__6p}></div>
+                      </div>
+                  </div>;
 
 const useAudio = url => {
   // let sound = new Audio(url);
@@ -31,7 +59,7 @@ const useAudio = url => {
 
   return (
     <div>
-        <button style={{'width': '2rem', 'height': 'auto', 'background': 'transparent', 'border': 'none', 'fill':'#F8F8F70' }} onClick={toggle}>{playing ? iconPause : iconPlay}</button>
+        <button style={{'position' : 'relative','width': '2rem', 'height': '2rem', 'background': 'transparent', 'border': 'none', 'fill':'#F8F8F70' }} onClick={toggle}>{playing ? iconPause : iconPlay}</button>
     </div>
   )
 };
