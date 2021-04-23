434dfa2675a7c005b41addcfa37b86c73b502cde
Will Mascarello <william.mascarello@duo.studio>
2021-04-23 18:55:21 -0300

    Adicionado fontes locais para melhorar a performac

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,9 +6,6 @@ export default class MyDocument extends Document {
       <Html lang="pt-br">
         <Head>
           <link rel="shortcut icon" href="logo.svg" type="image/svg"/>
-
-          <link rel="preload" as="style" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Stoke&display=swap" rel="stylesheet" type="text/css" />
         </Head>
         <body>
           <Main/>
